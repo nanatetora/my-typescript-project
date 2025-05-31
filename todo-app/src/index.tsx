fb5055f4e4c9d478b1fc0a5bfb1ebf9949f7e645
sironeco <shamrock.48697@gmail.com>
2025-05-31 16:23:28 +0900

    add:Todoアプリ_ひながた

---
+++ todo-app/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // 変更点
+import App from './App';
+
+// React 18 以降では createRoot を使う
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
